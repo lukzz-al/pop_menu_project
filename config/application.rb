99d7bf1286b2e9718cf57f6819088422bc9f4376
Lucas Almeida <lcs_al@outlook.com>
2024-10-20 20:26:32 -0300

    chore: applying rubocop on files

--- config/application.rb
+++ config/application.rb
@@ -5,21 +5,21 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module PopMenuProject
-  class Application < Rails::Application
-    config.load_defaults 8.0
-    config.autoload_lib(ignore: %w[assets tasks])
-    config.api_only = true
+	class Application < Rails::Application
+		config.load_defaults 8.0
+		config.autoload_lib(ignore: %w[assets tasks])
+		config.api_only = true
 
-    config.generators do |g|
-      g.test_framework :rspec,
-                       fixtures: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: true,
-                       request_specs: false
+		config.generators do |g|
+			g.test_framework :rspec,
+											fixtures: true,
+											view_specs: false,
+											helper_specs: false,
+											routing_specs: false,
+											controller_specs: true,
+											request_specs: false
 
-      g.fixture_replacement :factory_bot, dir: "spec/factories"
-    end
-  end
+			g.fixture_replacement :factory_bot, dir: "spec/factories"
+		end
+	end
 end
